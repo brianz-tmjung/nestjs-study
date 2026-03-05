9b23bfe38d6155665ee61c491a2ccfd93e8a53dc
tm.jung <tm.jung@brainz.co.kr>
2026-03-05 23:26:10 +0900

    feat : 스터디

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,26 +1,59 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET /posts
+  // 모든 post를 다 가져온다
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newsss',
-      title: '민지',
-      content: '메이킁ㅂ',
-      likeCount: 1000,
-      commentCount: 100,
-    };
+  getPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  // 2) GET /posts/:id
+  // id에 해당하는 post를 가져온다
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return this.postsService.getPostById(+id);
+  }
+
+  // 3) POST /posts
+  // 새로운 post를 생성한다
+  @Post()
+  postPost(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.postsService.createPost(author, title, content);
+  }
+
+  // 4) PUT /posts/:id
+  // id에 해당하는 post를 변경하거나 생성한다
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    return this.postsService.updatePost(+id, author, title, content);
+  }
+
+  // 5) DELETE /posts/:id
+  // id에 해당하는 post를 삭제한다
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(+id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'mj',
+    title: '뉴진스',
+    content: '메이크업',
+    likeCount: 1000,
+    commentCount: 4,
+  },
+  {
+    id: 2,
+    author: 'mj2',
+    title: '뉴진스2',
+    content: '메이크업3',
+    likeCount: 10040,
+    commentCount: 42,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  // 1) GET /posts
+  getAllPosts(): PostModel[] {
+    return posts;
+  }
+
+  // 2) GET /posts/:id
+  getPostById(id: number): PostModel {
+    const post = posts.find((post) => post.id === id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  // 3) POST /posts
+  createPost(author: string, title: string, content: string): PostModel {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts = [...posts, post];
+    return post;
+  }
+
+  // 4) PUT /posts/:id
+  updatePost(
+    id: number,
+    author?: string,
+    title?: string,
+    content?: string,
+  ): PostModel {
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+      if (author) post.author = author;
+      if (title) post.title = title;
+      if (content) post.content = content;
+      return post;
+    }
+    const newPost: PostModel = {
+      id,
+      author: author ?? '',
+      title: title ?? '',
+      content: content ?? '',
+      likeCount: 0,
+      commentCount: 0,
+    };
+    posts.push(newPost);
+    return newPost;
+  }
+
+  // 5) DELETE /posts/:id
+  deletePost(id: number): { id: number } {
+    const index = posts.findIndex((post) => post.id === id);
+    if (index === -1) {
+      throw new NotFoundException();
+    }
+    posts.splice(index, 1);
+    return { id };
+  }
+}
